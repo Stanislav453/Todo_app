436efc2d3c79b0c59e41b9c3bff34242f719963e
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-06-29 22:59:39 +0200

    Chage text welcomeMessage

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { HeaderStyle, HeaderUndergroundStyle } from "./styles/HeaderStyle";
 
 export const Header = () => {
-  const welcomeMessage = "Hellov Stanislav, what do you do today.";
+  const welcomeMessage = "What do you do today.";
   return (
     <HeaderStyle>
       <h1>{welcomeMessage}</h1>
